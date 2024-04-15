0e8ebc2344c51db9e6fe792018f7028eaa0c8f73
beq286 <beqisma@yandex.ru>
2024-04-15 23:44:55 +0300

    Added comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
- 
+
 int main()
 {
- std:: string name;
+  std::string name;
   std::cout << "Enter your name: ";
-  std::cin >> name;
+  std::cin >> name; // write here your name, please!
   std::cout << "Hello world from " << name << std::endl;
   return 0;
 }
